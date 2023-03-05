9a1cb729baede45159a27324713740fae5b96a07
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2023-03-05 19:43:06 +0330

    Created the angular version.

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss','../../../node_modules/@fortawesome/fontawesome-free/css/all.css']
+})
+
+export class LandingComponent implements OnInit {
+  focus: any;
+  focus1: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
