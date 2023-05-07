ef35a02774c0bd3a741a344073e65e8e75e7cf20
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2023-05-07 14:04:23 +0330

    Modified Menu.

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -13,7 +13,7 @@ export class CafeComponent implements OnInit {
         new Food('جوجه با استخوان ', '۲۲۰'),
         new Food('وزیری', '۲۲۰'),
         new Food('کباب دیگی', '۱۹۵'),
-        new Food('زرشک پلو', '۱۶۵'),
+        new Food('زرشک پلو با مرغ', '۱۶۵'),
         new Food('قیمه(روزهای زوج)', '۱۴۵'),
         new Food('قرمه‌سبزی(روزهای فرد)', '۱۴۵')];
 
@@ -32,26 +32,30 @@ export class CafeComponent implements OnInit {
         new Food('سوسیس تخم‌مرغ + چای', '۸۰'),
     ];
 
-    snacks = [new Food('سیب‌زمینی با سس قارچ و پنیر', '۱۴۰'),
+    snacks = [new Food('سیب‌زمینی با سس قارچ و پنیر', '۱۵۰'),
         new Food('چیپس و پنیر', '۱۳۰'),
         new Food('نان سیر ', '۱۰۰'),
         new Food('میرزاقاسمی ', '۱۱۰'),
         new Food('کشک بادمجون', '۱۰۰'),
     ];
 
-    mainCourse = [new Food('پاستا چیکن‌آلفردو', '۱۷۰'),
-        new Food('پاستا پستو', '۱۷۰'),
-        new Food('مک‌اندچیز ', '۱۸۰'),
+    mainCourse = [new Food('پاستا چیکن‌آلفردو', '۱۸۰'),
+        new Food('پاستا پستو', '۱۸۰'),
+        new Food('مک‌اندچیز ', '۱۹۰'),
         new Food('پنینی مرغ و پستو', '۱۶۰'),
         new Food('پنینی ژامبون', '۱۵۰'),
         new Food('بشقاب ویژه(دونفره)', '۳۵۰'),
-        new Food('بشقاب مرغ آلمانی', '۱۷۰'),
         new Food('اسپاگتی', '۱۵۰'),
         new Food('چیزبرگر', '۱۸۰'),
         new Food('لازانیا', '۱۷۰')
     ];
 
-    salads = [new Food('سالاد سزار', '۱۶۰'),
+    vegetarianFood = [new Food('لازانیا گیاهی', '۱۶۰'),
+        new Food('هات‌داگ گیاهی', '۱۸۰'),
+        new Food('پنینی ژامبون گیاهی', '۱۸۰'),
+    ];
+
+    salads = [new Food('سالاد سزار', '۱۷۰'),
         new Food('سالاد سرآشپز', '۱۷۰'),
     ];
 
@@ -71,7 +75,7 @@ export class CafeComponent implements OnInit {
     coffee = [new Food('اسپرسو دبل', '۵۵'),
         new Food('کاپوچینو', '۶۰'),
         new Food('آمریکانو ', '۵۸'),
-        new Food('لته', '۶۰'),
+        new Food('لته', '۶۵'),
         new Food('موکا', '۷۰'),
         new Food('کارامل ماکیاتو', '۷۰'),
         new Food('فرانسه', '۷۲'),
@@ -79,8 +83,8 @@ export class CafeComponent implements OnInit {
         new Food('قهوه ترک', '۶۳'),
     ];
 
-    coldDrinks = [new Food('لیموناد', '۶۵'),
-        new Food('موهیتو', '۶۵'),
+    coldDrinks = [new Food('لیموناد', '۶۸'),
+        new Food('موهیتو', '۷۵'),
         new Food('آیس تی لیمو ', '۶۵'),
         new Food('خیارسکنجبین', '۵۵'),
         new Food('سرویس آبجو', '۱۰۰')
@@ -91,10 +95,16 @@ export class CafeComponent implements OnInit {
         new Food('اسموتی ترش (انار + زرشک + لیمو + آلبالو) ', '۸۰')
     ];
 
-    sharbats = [new Food('ونوشه سرخ (گل‌گاوزبان + به‌لیمو + ختمی + بهار مرکبات)', '۶۰'),
-        new Food('کرشمه (نعنا فلفلی + ختمی + زرشک + گل نخود + لیمو)', '۶۰'),
+    sharbats = [new Food('ونوشه سرخ (گل‌گاوزبان + به‌لیمو + ختمی + بهار مرکبات)', '۷۰'),
+        new Food('کرشمه (نعنا فلفلی + ختمی + زرشک + گل نخود + لیمو)', '۷۰'),
         new Food('تبریز (زعفران + پسته + هل + بهارنارنج) ', '۷۰'),
         new Food('مهربانو (تخم‌گشنیز + کارامل + گل‌نخود +زیره سبز) ', '۷۰')
+    ];
+
+    cakes = [new Food('دبل چاکلت', '۸۵'),
+        new Food('چیزکیک کارامل', '۸۰'),
+        new Food('چیزکیک شاتوت ', '۸۰'),
+        new Food('کیک روز', '۸۰'),
     ];
 
 
